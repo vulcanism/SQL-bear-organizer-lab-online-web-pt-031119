d8cba48f74f673a16f839ddf7e08b38197342d2e
Savannah C <sgcorrell@gmail.com>
2019-05-14 17:46:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears (name, age, gender, color, temperament, alive)

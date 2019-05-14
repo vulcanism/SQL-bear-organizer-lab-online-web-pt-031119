46e25ddb7639a15681f8999315022f04cd1052d1
Savannah C <sgcorrell@gmail.com>
2019-05-14 17:50:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Mr. Chocolate, 4, male, brown, docile, 1)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Rowdy, 7, male, white, feisty, 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Tabitha, 9, female, black, lazy, 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Sergeant Brown, 17, male, brown, )
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Tabitha, 9, female, black, active, 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Sergeant Brown, 17, male, brown, lazy, 0)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES

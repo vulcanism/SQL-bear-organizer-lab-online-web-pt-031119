589e1b0d955b1257c86eb811792b55708d6a2e20
Savannah C <sgcorrell@gmail.com>
2019-05-14 18:05:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,17 +1,17 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = 'F'"
+  "SELECT name, age FROM bears WHERE gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name ASC"
+  "SELECT name FROM bears ORDER BY name ASC;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1"
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age

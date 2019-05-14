a7159e066e728b13af6a15c116d173027e9a04d2
Savannah C <sgcorrell@gmail.com>
2019-05-14 17:47:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Mr. Chocolate, 4, male, docile, )
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Mr. Chocolate, 4, male, docile, 1)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
